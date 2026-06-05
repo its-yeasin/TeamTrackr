768fb8ba65096bb50076cd903b8cec636bf48d6b
Yeasin Arafat <yeasinn2002@gmail.com>
2026-06-06 05:21:08 +0600

    Refactor DatabaseModule to use DATABASE_TOKEN and add global scope

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,16 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/node-postgres"
 import * as schema from './schema'
 
 
-export const DATABASE = "DATABASE"
+export const DATABASE_TOKEN = "DATABASE"
 
+@Global() // this module can be used over the whole project
 @Module({
     providers: [
         {
-            provide: DATABASE,
+            provide: DATABASE_TOKEN,
             useFactory(config: ConfigService) {
                 const connectionString = config.get<string>("DATABASE_URL")!;
                 const client = postgres(connectionString)
@@ -21,6 +22,7 @@ export const DATABASE = "DATABASE"
             },
             inject: [ConfigService]
         }
-    ]
+    ],
+    exports:[DATABASE_TOKEN]
 })
 export class DatabaseModule { }

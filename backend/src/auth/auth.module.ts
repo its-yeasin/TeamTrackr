26c52a6090bac167d5459ea06b6ed5a16d29a6f8
Yeasin Arafat <yeasinn2002@gmail.com>
2026-06-06 08:00:11 +0600

    Add AuthModule with JWT authentication setup and strategy

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { AuthService } from './auth.service';
+
+@Module({
+    imports: [
+        // Always use the same strategy (jwt) for all endpoints.
+        PassportModule.register({defaultStrategy: "jwt"}),
+
+        JwtModule.registerAsync({
+            useFactory: (config: ConfigService) =>({
+                secret: config.get<string>("JWT_SECRET")!,
+
+                signOptions: {
+                    // Tokens will expire after 15 minutes
+                    expiresIn: '15m'
+                }
+            }),
+            inject: [ConfigService]
+        })
+    ],
+    providers: [JwtStrategy, AuthService],
+    exports: [JwtStrategy, PassportModule],
+    controllers: []
+})
+export class AuthModule {}

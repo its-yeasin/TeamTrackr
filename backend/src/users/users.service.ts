06278d2dd969abeed55506ed49abb31f46c1cc87
Yeasin Arafat <yeasinn2002@gmail.com>
2026-06-21 14:55:24 +0600

    Implement Users module with controller, service, and tests

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ne } from 'drizzle-orm';
+import { ROLES } from 'src/common/constants';
+import type { TPgDatabase } from 'src/common/interfaces/db';
+import { DATABASE_TOKEN } from 'src/database/database.module';
+import { users } from 'src/database/schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@Inject(DATABASE_TOKEN) private readonly db: TPgDatabase) {}
+
+  async getAllUsers() {
+    const allUsers = await this.db
+      .select({
+        id: users.id,
+        name: users.name,
+        email: users.email,
+        role: users.role,
+        avatarUrl: users.avatarUrl,
+        createdAt: users.createdAt,
+        updatedAt: users.updatedAt,
+      })
+      .from(users)
+      .where(ne(users.role, ROLES.ADMIN));
+
+    return allUsers;
+  }
+}
